6581e34e617d54f5c95fde985b914cb153ab8028
Valerii Koshkin <valerii.koshkin@netcracker.com>
2021-09-17 00:37:36 +0400

    Added antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import 'antd/dist/antd.css';
 import App from './components/app';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
