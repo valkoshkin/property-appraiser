6ea56a140238d81ae0a0714c4d6e216d0c36cf27
Valerii Koshkin <valerii.koshkin@netcracker.com>
2021-09-17 22:42:39 +0400

    Updates

---
+++ precache-manifest.69dc3b9fe48b27feb8b358792d878da8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "facbfc38aca256889113e0ea32e697d7",
+    "url": "/property-appraiser/index.html"
+  },
+  {
+    "revision": "566e72684f71e9b5b9e3",
+    "url": "/property-appraiser/static/css/2.f6ed3ac0.chunk.css"
+  },
+  {
+    "revision": "2c25a8397d8f920e31c8",
+    "url": "/property-appraiser/static/css/main.8bef154b.chunk.css"
+  },
+  {
+    "revision": "566e72684f71e9b5b9e3",
+    "url": "/property-appraiser/static/js/2.61fd8d7f.chunk.js"
+  },
+  {
+    "revision": "9c3eda27a5152e65cb0f6ecd38b70659",
+    "url": "/property-appraiser/static/js/2.61fd8d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c25a8397d8f920e31c8",
+    "url": "/property-appraiser/static/js/main.61037820.chunk.js"
+  },
+  {
+    "revision": "a9242fa8895460bfde88",
+    "url": "/property-appraiser/static/js/runtime-main.75c4a366.js"
+  },
+  {
+    "revision": "20b420bacbdbabc0f993ff0cdd22a9cf",
+    "url": "/property-appraiser/static/media/cent-invest-logo.20b420ba.svg"
+  },
+  {
+    "revision": "8c1e57528a67df069ed75963ec3a9f60",
+    "url": "/property-appraiser/static/media/subheader-image.8c1e5752.png"
+  }
+]);
